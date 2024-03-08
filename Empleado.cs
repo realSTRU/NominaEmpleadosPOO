afa595cd054139bac0366fdcfa42d202ca098325
Cesar Reynoso <cesarreynoso0969@gmail.com>
2024-03-08 10:24:56 -0400

    Nomina Empleados .NET 5 For Learning

---
+++ Empleado.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nomina
+{
+    public class Empleado
+    {
+        const float AFP = 2.87f/100;
+        const float SFS = 3.04f/100;
+        public string nombre = "";
+        public string apellido = "";
+        public string cedula = "";
+        public string telefono = "";
+        public string direccion = "";
+        public string departamento = "";
+        public string puesto = "";
+        public float sueldo = 0;
+        public float sueldoNeto = 0;
+
+        public Empleado(string nombre, string apellido, string cedula, string telefono, string direccion,
+            string departamento, string puesto, float sueldo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.cedula = cedula;
+            this.telefono = telefono;
+            this.direccion = direccion;
+            this.departamento = departamento;
+            this.puesto = puesto;
+            this.sueldo = sueldo;
+        }
+
+        public string GetCedula()
+        {
+            return this.cedula;
+        }
+
+        public float GetAFP()
+        {
+            return sueldo * AFP;
+        }
+
+        public float GetSFS()
+        {
+            return sueldo * SFS;
+        }
+
+        public float GetSIR()
+        {
+            float anual = sueldo*12;
+            if(anual<=416220){
+                return 0;
+            }
+            if(anual>416220 && anual <=624329){
+                return (((anual-416220.01f)*0.15f)/12);
+            }
+            if(anual>624329 && anual <=867123){
+                return (((((anual-624329.01f)*0.2f))+31216)/12);
+            }
+            if(anual>867123){
+                return (((((anual-867123.01f)*0.25f))+79776)/12);
+            }
+
+            return 1;
+        }
+
+        public float GetSueldoNeto()
+        {
+            return sueldoNeto = sueldo-(GetAFP() + GetSFS() + GetSIR());
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Nombre: {0}\nApellido: {1}\nCedula: {2}\nTelefono: {3}"
+                +"\nDireccion: {4}\nDepartemento: {5}\nPuesto: {6}\nSueldo: {7}\nAFP: {8}\nSFS: {9}\nISR: {10}"
+                +"\nSueldo Neto: {11}",nombre,apellido,cedula,telefono,direccion,departamento,puesto,sueldo,GetAFP(),GetSFS(),GetSIR(),sueldoNeto);
+            return (sb.ToString());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Nomina
+{
+    class Program
+    {
+        static List<Empleado> empleados = new List<Empleado>();
+
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+            string valor = "";
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("~~ Menu de Nomina ~~");
+                Console.WriteLine("\n1. Registrar Empleado\n2.Borrar Empleado\n3.Actualizar Empleado\n4.Buscar Empleado\n5. Listar Nomina\n6. Salir");
+                Console.Write("\nOpcion: ");
+                valor = Console.ReadLine();
+                opcion = Convert.ToInt32(valor);
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Clear();
+                        RegistrarEmpleado();
+                        Console.Write("\n\nPresione ENTER para continuar...");
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        BorrarEmpleado();
+                        Console.Write("\n\nPresione ENTER para continuar...");
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        ActualizarEmpleado();
+                        Console.Write("\n\nPresione ENTER para continuar...");
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        BuscarEmpleado();
+                        Console.Write("\n\nPresione ENTER para continuar...");
+                        Console.ReadLine();
+                        break;
+                    case 5:
+                        Console.Clear();
+                        ListarNomina();
+                        Console.Write("\n\nPresione ENTER para continuar...");
+                        Console.ReadLine();
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        Console.WriteLine("\n\nOpcion incorrecta!");
+                        if (opcion != 3)
+                            Console.Write("\n\nPresione ENTER para continuar...");
+                        Console.ReadLine();
+                        break;
+
+                }
+            } while (opcion != 3 || opcion < 3 && opcion > 0);
+        }
+
+        public static void RegistrarEmpleado()
+        {
+            string nombre = "";
+            string apellido = "";
+            string cedula = "";
+            string telefono = "";
+            string direccion = "";
+            string departamento = "";
+            string puesto = "";
+            float sueldo = 0;
+            string valor = "";
+
+            Console.WriteLine("~~ Registrar Empleado ~~");
+
+            Console.Write("Digite los nombres: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Digite los apellidos: ");
+            apellido = Console.ReadLine();
+
+            Console.Write("Digite la cedula: ");
+            cedula = Console.ReadLine();
+
+            Console.Write("Digite el numero de telefono: ");
+            telefono = Console.ReadLine();
+
+            Console.Write("Digite la direccion: ");
+            direccion = Console.ReadLine();
+
+            Console.Write("Digite el departamento: ");
+            departamento = Console.ReadLine();
+
+            Console.Write("Digite el puesto: ");
+            puesto = Console.ReadLine();
+
+            Console.Write("Digite el sueldo: ");
+            valor = Console.ReadLine();
+            sueldo = Convert.ToSingle(valor);
+
+            Empleado empleado = new Empleado(nombre,apellido, cedula, telefono, direccion, departamento, puesto, sueldo);
+            empleado.GetSueldoNeto();
+            empleados.Add(empleado);
+            Console.WriteLine("Empleado Registrado!");
+        }
+        static void BuscarEmpleado()
+        {
+            Console.WriteLine("Ingrese la cédula del empleado a buscar:");
+            string cedula = Console.ReadLine();
+            var r = empleados.Find(e => e.cedula == cedula);
+            Empleado empleadoEncontrado = r;
+
+            if (empleadoEncontrado != null)
+            {
+                Console.WriteLine("Empleado encontrado:");
+                Console.WriteLine(empleadoEncontrado);
+            }
+            else
+            {
+                Console.WriteLine("Empleado no encontrado.");
+            }
+        }
+
+        static void BorrarEmpleado()
+        {
+            Console.WriteLine("Ingrese la cédula del empleado a borrar:");
+            string cedula = Console.ReadLine();
+
+            Empleado empleadoBorrar = empleados.Find(empleado => empleado.cedula == cedula);
+
+            if (empleadoBorrar != null)
+            {
+                empleados.Remove(empleadoBorrar);
+                Console.WriteLine("Empleado borrado correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("Empleado no encontrado.");
+            }
+        }
+
+        static void ActualizarEmpleado()
+        {
+            Console.WriteLine("Ingrese la cédula del empleado a actualizar:");
+            string cedula = Console.ReadLine();
+
+            Empleado empleadoActualizar = empleados.Find(empleado => empleado.cedula == cedula);
+
+            if (empleadoActualizar != null)
+            {
+                Console.WriteLine("Ingrese el nuevo nombre:");
+                empleadoActualizar.nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese los nuevos apellidos:");
+                empleadoActualizar.apellido = Console.ReadLine();
+                Console.WriteLine("Ingrese la nueva dirección:");
+                empleadoActualizar.direccion = Console.ReadLine();
+                Console.WriteLine("Ingrese el nuevo teléfono:");
+                empleadoActualizar.telefono = Console.ReadLine();
+                Console.WriteLine("Ingrese el nuevo departamento:");
+                empleadoActualizar.departamento = Console.ReadLine();
+                Console.WriteLine("Ingrese el nuevo puesto:");
+                empleadoActualizar.puesto = Console.ReadLine();
+
+                Console.WriteLine("Empleado actualizado correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("Empleado no encontrado.");
+            }
+        }
+
+        public static void ListarNomina()
+        {
+            Console.WriteLine("NOMINA DE EMPLEADOS");
+            foreach (Empleado e in empleados)
+            {
+                Console.WriteLine("\n\n"+e.ToString());
+            }
+        }
+    }
+}
